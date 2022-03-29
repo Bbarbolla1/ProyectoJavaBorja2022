3ad654b64da4294e3cb5db5217124af4958729dd
Bbarbolla1 <101900220+Bbarbolla1@users.noreply.github.com>
2022-03-29 19:01:42 +0200

    Update Main.java

--- src/main/java/RepositorioBorja2022/Main.java
+++ src/main/java/RepositorioBorja2022/Main.java
@@ -13,7 +13,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       System.out.println("Hola pon aqui un nombre- y escribe algo más");
+       System.out.println("Hola pon aqui un nombre- y escribe algo más por favor");
 
     }
     
